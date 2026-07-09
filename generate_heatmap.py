9e9791f3226a29df8612db50b28e742b8c70f3b7
Luca <lucajamesday@gmail.com>
2026-07-09 17:10:35 +0100

    UX: Set both heatmap and individual pin layers to show by default upon loading

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -22,9 +22,9 @@ gdf = gdf.to_crs(epsg=4326)
 # Create the base map
 uk_map = folium.Map(location=[55.3781, -3.4360], zoom_start=6)
 
-# --- THE UPGRADE: Create separate toggleable layers ---
-heatmap_layer = folium.FeatureGroup(name="Capacity Heatmap (Density)")
-pin_layer = folium.FeatureGroup(name="Individual Sites (Pins)", show=False) # show=False hides it by default
+# --- THE FIX: Set both layers to show=True by default ---
+heatmap_layer = folium.FeatureGroup(name="Capacity Heatmap (Density)", show=True)
+pin_layer = folium.FeatureGroup(name="Individual Sites (Pins)", show=True) 
 
 heat_data = []
 marker_cluster = MarkerCluster().add_to(pin_layer)
@@ -36,16 +36,25 @@ for index, row in gdf.iterrows():
         lat = row.geometry.y
         capacity = float(row['Installed Capacity (MWelec)']) if pd.notnull(row['Installed Capacity (MWelec)']) else 0.1
         site_name = row['Site Name']
-        tech_type = row['Technology Type']
+        tech_type = str(row['Technology Type'])
         
         # 1. Prep Heatmap Data
         heat_data.append([lat, lon, capacity])
         
-        # 2. Add Pins to the Pin Layer
+        # --- NEW FLAIR: Color-code the pins based on technology ---
+        if 'Wind' in tech_type:
+            pin_color = 'blue'
+        elif 'Solar' in tech_type:
+            pin_color = 'orange'
+        else:
+            pin_color = 'green'
+            
+        # 2. Add Pins to the Pin Layer with the custom color icon
         popup_text = f"<b>Site:</b> {site_name}<br><b>Type:</b> {tech_type}<br><b>Capacity:</b> {capacity} MW"
         folium.Marker(
             location=[lat, lon],
-            popup=folium.Popup(popup_text, max_width=300)
+            popup=folium.Popup(popup_text, max_width=300),
+            icon=folium.Icon(color=pin_color, icon='info-sign')
         ).add_to(marker_cluster)
         
     except Exception:
@@ -60,6 +69,27 @@ pin_layer.add_to(uk_map)
 
 # --- THE MAGIC MENU: Add a control box so the user can check/uncheck layers ---
 folium.LayerControl().add_to(uk_map)
+
+# --- THE UPGRADE: Inject a custom HTML Legend ---
+legend_html = '''
+<div style="
+    position: fixed; 
+    bottom: 50px; left: 50px; width: 130px; height: 110px; 
+    border:2px solid grey; z-index:9999; font-size:14px;
+    background-color:white; padding: 10px; border-radius: 5px;
+    ">
+    <b>Energy Type</b><br>
+    <i class="fa fa-map-marker fa-1.5x" style="color:blue"></i> Wind<br>
+    <i class="fa fa-map-marker fa-1.5x" style="color:orange"></i> Solar<br>
+    <i class="fa fa-map-marker fa-1.5x" style="color:green"></i> Other
+</div>
+'''
+uk_map.get_root().html.add_child(folium.Element(legend_html))
+# ------------------------------------------------
+
+# Save the final map
+uk_map.save("index.html")
+print("Success! Layered map with legend generated. Refresh index.html to view.")
 
 uk_map.save("index.html")
 print("Success! Layered map generated. Refresh index.html to view.")
